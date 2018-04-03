d4b57df6ad39c8764acfe8598fda4b22a3a906c9
Alessandro Norfo <sprintingkiwi@gmail.com>
2018-04-03 07:03:00 +0200

    added Lizards Playing Cards skit

---
+++ Assets/Scripts/Skit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skit : MonoBehaviour
+{
+    public Encounter encounter;
+    GameController gc;
+
+    void Start()
+    {
+        gc = GameObject.Find("Game Controller").GetComponent<GameController>();
+    }
+
+    private void Update()
+    {
+        Jrpg.AdjustSortingOrder(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (Debug.isDebugBuild)
+            Debug.Log("Triggered skit " + name);
+
+        if (other.gameObject.name == "Player")
+            gc.StartCoroutine(gc.TriggerBattle(encounter, name));
+    }
+}
