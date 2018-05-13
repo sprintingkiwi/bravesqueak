b8b9e6f9978330c7e1dcfa9b93130dd0416b5b42
Alessandro Norfo <sprintingkiwi@gmail.com>
2018-05-13 18:32:38 +0200

    .

---
+++ Assets/WebPlayerTemplates/Deprecated/Ignite.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ignite : Skill
+{
+    public override IEnumerator OngoingEffect(Battler target)
+    {
+        yield return StartCoroutine(base.OngoingEffect(target));
+
+        yield return StartCoroutine(Effect(target));
+    }
+}
