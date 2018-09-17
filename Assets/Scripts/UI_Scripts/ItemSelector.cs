7aa6e29eb4a9c2022ab5c8384b2704916914aaf1
Alessandro Norfo <sprintingkiwi@gmail.com>
2018-09-17 18:41:58 +0200

    added item selector script

---
+++ Assets/Scripts/UI_Scripts/ItemSelector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSelector : MonoBehaviour
+{
+    Item selectedItem;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void OnMouseDown()
+    {
+        Jrpg.Log("Intantiating Item Selection Menu");
+        GameObject selMenu = Instantiate(Resources.Load("Menu/ItemSelectionMenu") as GameObject);
+
+    }
+}
