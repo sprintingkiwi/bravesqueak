0dfd340857fa12e90ef86c896732e878cfa0275c
Alessandro Norfo <sprintingkiwi@gmail.com>
2020-10-20 12:44:30 +0200

    added exit menu

---
+++ Assets/Scripts/UI_Scripts/ExitMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitMenu : MonoBehaviour
+{
+    [Header("Drag and Drop")]
+    public Transform buttonsContainer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (buttonsContainer.gameObject.activeSelf)
+                buttonsContainer.gameObject.SetActive(false);
+            else
+                buttonsContainer.gameObject.SetActive(true);
+        }
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
