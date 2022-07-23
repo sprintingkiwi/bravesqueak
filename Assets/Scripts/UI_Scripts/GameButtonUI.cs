a1eb1f00e510fafc6aad0b812a33f967855e6b35
Alessandro Norfo <sprintingkiwi@gmail.com>
2022-07-23 13:50:44 +0200

    created UI game buttons

---
+++ Assets/Scripts/UI_Scripts/GameButtonUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameButtonUI : MonoBehaviour
+{
+    bool IsButtonAvailable(string btnName)
+    {
+        try
+        {
+            Input.GetButton(btnName);
+            return true;
+        }
+        catch (UnityException exc)
+        {
+            return false;
+        }
+    }
+
+    public void ButtonDown()
+    {
+        Debug.Log(name + " touched down");
+        InputManager.instance.ProcessButtonTouch(name, "Down");
+    }
+
+    public void ButtonUp()
+    {
+        Debug.Log(name + " touched up");
+        InputManager.instance.ProcessButtonTouch(name, "Up");
+    }
+}
