6505d4f58e67365edcc380bd022ee965aeb035ca
Alessandro Norfo <sprintingkiwi@gmail.com>
2022-07-23 10:08:40 +0200

    ADDED ASPECT RATIO CONTROL

--- Assets/Scripts/Map_Scripts/MapCameraController.cs
+++ Assets/Scripts/Map_Scripts/MapCameraController.cs
@@ -13,17 +13,37 @@ public class MapCameraController : BattleCameraController
     public SpriteRenderer spriteBounds;
     public bool followPlayer;
 
+    Camera cam;
+
     // Use this for initialization
     public override void Start()
     {
         base.Start();
+        cam = gameObject.GetComponent<Camera>();
     }
 
     public void Setup(WorldMap map)
     {
+        float screenRatio = Screen.width / Screen.height;
+        float desiredRatio = 16f / 9f;
+        float height = Screen.height;
+        float width = Screen.width;
+        if (screenRatio > desiredRatio)
+        {
+            height = Screen.height;
+            width = (height * 16f) / 9f;
+        }
+        else if (screenRatio < desiredRatio)
+        {
+            width = Screen.width;
+            height = (width * 9f) / 16f;
+        }
+
+
+
         spriteBounds = map.cameraBoundary;
-        float vertExtent = gameObject.GetComponent<Camera>().orthographicSize;
-        float horzExtent = vertExtent * Screen.width / Screen.height;
+        float vertExtent = cam.orthographicSize;
+        float horzExtent = vertExtent * width / height; //Screen.width / Screen.height;
         leftBound = spriteBounds.transform.position.x + (horzExtent - spriteBounds.sprite.bounds.size.x / 2.0f);
         rightBound = spriteBounds.transform.position.x + (spriteBounds.sprite.bounds.size.x / 2.0f - horzExtent);
         bottomBound = spriteBounds.transform.position.y + (vertExtent - spriteBounds.sprite.bounds.size.y / 2.0f);
@@ -41,7 +61,7 @@ public class MapCameraController : BattleCameraController
             FollowPlayer();
     }
 
-    void FollowPlayer ()
+    void FollowPlayer()
     {
         Vector3 pos = new Vector3(target.position.x, target.position.y, transform.position.z);
         pos.x = Mathf.Clamp(pos.x, leftBound, rightBound);
