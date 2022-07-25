69bffa99a05f6747eed5775cd10000aaae7a7b49
Alessandro Norfo <sprintingkiwi@gmail.com>
2022-07-25 18:20:33 +0200

    removed touch controls on main menu and disabled areastuff on gamestart

--- Assets/Scripts/UI_Scripts/TouchscreenOnly.cs
+++ Assets/Scripts/UI_Scripts/TouchscreenOnly.cs
@@ -1,10 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TouchscreenOnly : MonoBehaviour
-{    void Start()
+{    
+    void Start()
     {
         gameObject.SetActive(GameController.Instance.touchControls);
+        if (gameObject.activeSelf) StartCoroutine(CheckMainMenu());
+    }
+
+    IEnumerator CheckMainMenu()
+    {
+        Vector3 orignalScale = transform.localScale;
+        if (GameController.Instance.situation == "")
+            transform.localScale = Vector3.zero;
+        while (GameController.Instance.situation == "")
+            yield return null;
+        transform.localScale = orignalScale;
     }
 }
