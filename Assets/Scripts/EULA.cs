768c10f6ed460940d9721433f20636fb70b529ae
Alessandro Norfo <sprintingkiwi@gmail.com>
2022-07-29 11:09:26 +0200

    added EULA

---
+++ Assets/Scripts/EULA.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnityEngine.SceneManagement;
+
+
+public class EULA : MonoBehaviour
+{
+    [System.Serializable]
+    class EULAConsent
+    {
+        public string date;
+        public string answer;
+    }
+    EULAConsent eulaConsent;
+    FileStream saveFile;
+    BinaryFormatter bf;
+    string consentPath;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        consentPath = Application.persistentDataPath + "/EULA_Consent.dat";
+        Debug.Log(consentPath);
+        bf = new BinaryFormatter();
+
+        if (!File.Exists(consentPath))
+        {
+            eulaConsent = new EULAConsent
+            {
+                date = Time.time.ToString(),
+                answer = "Not Accepted"
+            };
+            SaveEULAConsent();
+        }
+        else
+        {
+            eulaConsent = GetEULAConsent();
+            if (eulaConsent != null)
+                if (eulaConsent.answer == "Accepted")
+                    GoOn();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    EULAConsent GetEULAConsent()
+    {
+        saveFile = File.Open(consentPath, FileMode.Open);
+        if (saveFile.Length > 0)
+        {
+            eulaConsent = (EULAConsent)bf.Deserialize(saveFile);
+            saveFile.Close();
+            Debug.Log("DATE: " + eulaConsent.date + "; ANSWER: " + eulaConsent.answer);
+            return eulaConsent;
+        }
+        else
+        {
+            Debug.LogWarning("EULA save file is empty. This should not happen. Returning null.");
+            return null;
+        }
+    }
+
+    void SaveEULAConsent()
+    {
+        saveFile = File.Open(consentPath, FileMode.OpenOrCreate);
+        bf.Serialize(saveFile, eulaConsent);
+        saveFile.Close();
+    }
+
+    public void Accepted()
+    {
+        eulaConsent.date = Time.time.ToString();
+        eulaConsent.answer = "Accepted";
+        SaveEULAConsent();
+        GoOn();
+    }
+
+    public void NotAccepted()
+    {
+        Application.Quit();
+    }
+
+    void GoOn()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Assets/Scripts/UI_Scripts/ScrollController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class ScrollController : MonoBehaviour
+{
+    [Header("Options")]
+    public float contentEndY;
+    public float maxContentY;
+    public UnityEvent[] contentReadEvents;
+
+    [Header("Drag and Drop")]
+    public RectTransform content;
+
+    bool readEventTriggered = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        content.anchoredPosition = Vector2.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckContentRead();
+        RestrainContentPosition();
+    }
+
+    void CheckContentRead()
+    {
+        if (content.anchoredPosition.y > contentEndY && !readEventTriggered)
+        {
+            foreach (UnityEvent readEvent in contentReadEvents) readEvent.Invoke();
+            readEventTriggered = true;
+        }
+    }
+
+    void RestrainContentPosition()
+    {
+        if (content.anchoredPosition.y < 0)
+            content.anchoredPosition = Vector2.zero;
+        else if (content.anchoredPosition.y > maxContentY)
+            content.anchoredPosition = new Vector2(content.anchoredPosition.x, maxContentY);
+    }
+}
